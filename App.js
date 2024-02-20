b0ae61d40ccceb62038814fbb615ff09e9e4d4f0
Singhakshat0205 <akshatsingh52002@gmail.com>
2024-02-20 22:58:28 +0530

    update

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
 const parent= React.createElement("div",
 {id:"parent"},
 React.createElement(
-"div"
-,{id:"child"},
+"div" 
+,{id:"child"}, 
 [React.createElement("h1",{},"i am a heading"), 
 React.createElement("h2",{},"hi i am small heading")]
 ));
